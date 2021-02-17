79b05d2017dadc88937ae8a3ac0d359ffc575f36
Josh Allan <allan.josh07@gmail.com>
2021-02-17 11:36:28 -0500

    Refactored the reducers and actions into their own folders/files for cleaner code.

---
+++ client/src/actions/index.js
@@ -0,0 +1,4 @@
+// the actions describe what we want to do in simple terms called types
+const SEARCH = () => {
+  return { type: 'SEARCH' };
+};
--- client/src/index.js
+++ client/src/index.js
@@ -2,30 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer from './reducers';
 
+// redux store
 import { createStore } from 'redux';
+const store = createStore(reducer);
 
-// create the store for globalized state
-let store = createStore(reducer);
-
-// create the actions -- describes what we want to do
-const SEARCH = () => {
-  return { type: 'SEARCH' };
-};
-
-// create the reducer -- based on action called, it will change state
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    // if the SEARCH action is dispatched, then spread the new query to the globalized state (in store)
-    case 'SEARCH':
-      return [...state, { id: action.id, query: action.text }];
-    // else, return the globalized state (in store) as it currently is
-    default:
-      return state;
-  }
-};
-
-// dispatch -- actually sends the action to the reducer
+// display in console
+store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.render(
   <React.StrictMode>
