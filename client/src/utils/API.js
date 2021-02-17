3ac72485ee88498a6be8846c78eb2eecc572125e
Josh Allan <allan.josh07@gmail.com>
2021-02-17 15:09:12 -0500

    Updated the api call to fix cors issues.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 // eslint-disable-next-line
 export default {
   getSearchResults: function (search) {
-    return axios.get(`http://hn.algolia.com/api/v1/search?query=${search}`);
+    return axios.get(`https://hn.algolia.com/api/v1/search?query=${search}`);
   }
 };
